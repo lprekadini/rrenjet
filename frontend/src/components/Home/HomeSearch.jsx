e94045eb837d592dfe205562d1a7c8c57a7aaeb0
Liridon Prekadini <lprekadini1@gmail.com>
2025-02-24 21:36:58 +0100

    update text

--- frontend/src/components/Home/HomeSearch.jsx
+++ frontend/src/components/Home/HomeSearch.jsx
@@ -25,10 +25,10 @@ export default function HomeSearch({img}) {
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
               <div className="mx-auto max-w-2xl text-center">
                 <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
-                  Mëso rrënjët tua
+                Zbulo Rrënjët e Tua
                 </h1>
                 <p className="mt-2 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
-                  Eksploro historin e mahnitshme te shqipëtarëve.
+                Eksploro historinë e pasur dhe frymëzuese të personaliteteve shqiptare.
                 </p>
                 <SearchInput />
                 {/* <div className="mt-10 flex items-center justify-center gap-x-6">
